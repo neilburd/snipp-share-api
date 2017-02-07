3f7fe8de92dd26f3779e8d22343962fd5e077e6f
“neilburd” <neilburd@gmail.com>
2017-02-07 15:07:30 -0500

    installed the devise_token_auth and am trying to get it working

--- db/migrate/20170207193156_snippet_creation_migration.rb
+++ db/migrate/20170207193156_snippet_creation_migration.rb
@@ -1,8 +1,9 @@
-class CreateSnippets < ActiveRecord::Migration[5.0]
+class SnippetCreationMigration < ActiveRecord::Migration[5.0]
   def change
     create_table :snippets do |t|
       t.string :title
       t.text :code
+      t.string :language
       t.references :user, foreign_key: true
 
       t.timestamps
